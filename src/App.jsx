69653d659ed2b76f1a454351a81a0764e184daa2
Harry <harrygillfdk50@gmail.com>
2026-05-05 22:43:25 -0400

    feat: wire all sections in final layout order with Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Footer,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
+const App = () => (
+  <BrowserRouter>
+    <div className="relative z-0 bg-primary min-h-screen">
+      <div className="relative">
+        <Navbar />
+        <Hero />
       </div>
-    </BrowserRouter>
-  );
-}
+      <About />
+      <Feedbacks />
+      <Tech />
+      <Works />
+      <Experience />
+      <div className="relative z-0">
+        <Contact />
+      </div>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
